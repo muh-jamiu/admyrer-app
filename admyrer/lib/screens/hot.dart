28030d3c19a9d8e06e6b71cf6b5ed81c17a253c0
muh-jamiu <ganiujamiu03@gmail.com>
2024-06-20 22:05:15 +0100

    fixes

--- admyrer/lib/screens/hot.dart
+++ admyrer/lib/screens/hot.dart
@@ -10,6 +10,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:admyrer/services/api_service.dart';
 import 'dart:convert';
 import 'package:admyrer/screens/allUsers.dart';
+import 'package:admyrer/screens/single.dart';
 // import "package:Admyrer/widget/background.dart";
 
 class Hot extends StatefulWidget {
@@ -229,8 +230,14 @@ class Users extends StatefulWidget {
 }
 
 class _UsersState extends State<Users> {
+
   @override
   Widget build(BuildContext context) {
+    
+  void goSingle(user) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Single(users: user)));
+  }
+
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
@@ -244,37 +251,40 @@ class _UsersState extends State<Users> {
                       const SizedBox(
                         height: 10,
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                                color: const Color.fromARGB(255, 207, 37, 212),
-                                width: 2.0)),
-                        child: Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: Container(
-                            height: 55,
-                            width: 55,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(50),
-                              child: FadeInImage.assetNetwork(
-                                  placeholder:
-                                      "assets/images/no_profile_image.webp",
-                                  image: user.avatar,
-                                  height: 55,
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                  imageErrorBuilder: (BuildContext context,
-                                      Object error, StackTrace? stackTrace) {
-                                    return Image.asset(
-                                      'assets/images/no_profile_image.webp',
-                                      height: 55,
-                                      fit: BoxFit.cover,
-                                      width: double.infinity,
-                                    );
-                                  }),
+                      InkWell(
+                        onTap: () => goSingle(user),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  color: const Color.fromARGB(255, 207, 37, 212),
+                                  width: 2.0)),
+                          child: Padding(
+                            padding: EdgeInsets.all(2.0),
+                            child: Container(
+                              height: 55,
+                              width: 55,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: FadeInImage.assetNetwork(
+                                    placeholder:
+                                        "assets/images/no_profile_image.webp",
+                                    image: user.avatar,
+                                    height: 55,
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                    imageErrorBuilder: (BuildContext context,
+                                        Object error, StackTrace? stackTrace) {
+                                      return Image.asset(
+                                        'assets/images/no_profile_image.webp',
+                                        height: 55,
+                                        fit: BoxFit.cover,
+                                        width: double.infinity,
+                                      );
+                                    }),
+                              ),
                             ),
                           ),
                         ),
---
+++ admyrer/lib/screens/single.dart
@@ -0,0 +1,421 @@
+import 'package:admyrer/widget/backgrounds.dart';
+import 'package:admyrer/widget/gradient_button.dart';
+import 'package:admyrer/widget/bootstrap_textfield.dart';
+import 'package:admyrer/widget/google_login.dart';
+import 'package:admyrer/widget/facebook.dart';
+import 'package:flutter/material.dart';
+import 'package:admyrer/services/api_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:convert';
+import 'package:admyrer/models/user.dart';
+
+class Single extends StatefulWidget {
+  final UserModel users;
+  const Single({super.key, required this.users});
+
+  @override
+  State<Single> createState() => _SingleState();
+}
+
+class _SingleState extends State<Single> {
+  final ApiService _apiService = ApiService();
+  late UserModel user = widget.users;
+  bool isLoading = true;
+
+  void showErrorToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.TOP_RIGHT,
+      timeInSecForIosWeb: 5,
+      textColor: Colors.white,
+      backgroundColor: Colors.pink[300],
+      fontSize: 15.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      @override
+      void initState() {
+        super.initState();
+        setState(() {
+          user = widget.users;
+        });
+      }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          // padding: const EdgeInsets.all(15.0),
+          children: [
+            const Backgrounds(),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () => {
+                               Navigator.pop(context)
+                            },
+                            child: Icon(Icons.arrow_back, size: 25)
+                            ),
+                            const SizedBox(width: 15),
+                          Text(
+                            user.firstName,
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.more_vert_outlined,
+                            color: Colors.pink[300],
+                          ),
+                          const SizedBox(width: 15),
+                          Icon(Icons.diamond_rounded, color: Colors.blue[300]),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ), Settings(user: user),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Expanded(child: MyGridList())
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  final UserModel user;
+  const Settings({super.key, required this.user});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    var firstName = widget.user.firstName;
+    var lastName = widget.user.lastName;
+    var country = widget.user.country;
+    var state = widget.user.state ?? "N/A";
+    var avatar = widget.user.avatar;
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                    color: const Color.fromARGB(255, 207, 37, 212),
+                    width: 2.0)),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                height: 115,
+                width: 115,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/no_profile_image.webp",
+                      image: avatar,
+                      height: 115,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      imageErrorBuilder: (BuildContext context, Object error,
+                          StackTrace? stackTrace) {
+                        return Image.asset(
+                          'assets/images/no_profile_image.webp',
+                          height: 115,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        );
+                      }),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Center(
+            child: Text(
+          '$firstName $lastName',
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        )),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.location_on_outlined,
+              color: Colors.pink[300],
+            ),
+            const SizedBox(
+              width: 2,
+            ),
+            Text(
+              '$state, $country',
+              style: const TextStyle(
+                  fontSize: 15, color: Color.fromARGB(255, 63, 63, 63)),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Center(
+          child: InkWell(
+            onTap: () => {},
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              width: 130,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(23, 233, 30, 98),
+                  borderRadius: BorderRadius.circular(5)),
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.edit_note_outlined,
+                    color: Colors.pink,
+                    size: 25,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Edit Profile",
+                    style: TextStyle(color: Colors.pink),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.heart_broken_sharp,
+                    color: Color.fromARGB(255, 63, 63, 63)),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "10 Likes",
+                  style: TextStyle(
+                      fontSize: 15, color: Color.fromARGB(255, 63, 63, 63)),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.remove_red_eye,
+                    color: Color.fromARGB(255, 63, 63, 63)),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "10 Visits",
+                  style: TextStyle(
+                      fontSize: 15, color: Color.fromARGB(255, 63, 63, 63)),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.share,
+                  color: Color.fromARGB(255, 63, 63, 63),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Share",
+                  style: TextStyle(
+                      fontSize: 15, color: Color.fromARGB(255, 63, 63, 63)),
+                ),
+              ],
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class MyGridList extends StatefulWidget {
+  const MyGridList({super.key});
+
+  @override
+  State<MyGridList> createState() => _MyGridListState();
+}
+
+class _MyGridListState extends State<MyGridList> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      crossAxisSpacing: 10,
+      childAspectRatio: 3 / 3,
+      mainAxisSpacing: 10,
+      children: [
+        InkWell(
+          onTap: () => {},
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(35, 155, 39, 176),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.replay_outlined,
+                  color: Colors.purple,
+                  size: 25,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Live")
+              ],
+            ),
+          ),
+        ),
+
+        InkWell(
+          onTap: () => {},
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(35, 155, 39, 176),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.replay_outlined,
+                  color: Colors.purple,
+                  size: 25,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Live")
+              ],
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () => {},
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(35, 155, 39, 176),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.replay_outlined,
+                  color: Colors.purple,
+                  size: 25,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Live")
+              ],
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () => {},
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(35, 155, 39, 176),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.replay_outlined,
+                  color: Colors.purple,
+                  size: 25,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Live")
+              ],
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () => {},
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(35, 155, 39, 176),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.replay_outlined,
+                  color: Colors.purple,
+                  size: 25,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Live")
+              ],
+            ),
+          ),
+        ),
+       ],
+    );
+  }
+}
