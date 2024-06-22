7b966d1eabce6bca7b14afacc264814176c6aedf
muh-jamiu <ganiujamiu03@gmail.com>
2024-06-22 16:45:16 +0100

    fixes

--- admyrer/lib/widget/bottom_open.dart
+++ admyrer/lib/widget/bottom_open.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 void showCustomBottomSheet(BuildContext context) {
+  bool isBoys = false;
+  bool isGirls = false;
+  bool isBoth = false;
+
+  TextEditingController _searchController = TextEditingController();
+  
   showModalBottomSheet<void>(
     context: context,
     builder: (BuildContext context) {
@@ -11,10 +17,60 @@ void showCustomBottomSheet(BuildContext context) {
           color: Colors.pink[400],
         ),
         height: 500,
-        child: const Center(
-          child: Text('This is a persistent bottom sheet'),
+        child:  Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SwitchListTile(
+                title: Text('Boys'),
+                value: isBoys,
+                onChanged: (bool value) {
+                },
+              ),
+              SwitchListTile(
+                title: Text('Girls'),
+                value: isGirls,
+                onChanged: (bool value) {
+                },
+              ),
+              SwitchListTile(
+                title: Text('Both'),
+                value: isBoth,
+                onChanged: (bool value) {
+                },
+              ),
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle search action
+                    },
+                    child: Text('Search'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text('Reset'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
         ),
       );
     },
   );
 }
+
+
